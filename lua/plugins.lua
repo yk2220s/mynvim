9f7b8528728176eba4f9a817492c663e18439042
岸本優一 <yk2220s@yk2220s-macmini23.local>
2023-07-23 01:41:09 +0900

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -42,7 +42,7 @@ return {
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { 'go', 'lua', 'vim' },
+        ensure_installed = { 'go', 'lua', 'vim', 'sql' },
         auto_install = false,
         highlight = { enable = true },
         indent = { enable = true },
