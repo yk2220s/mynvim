402fa9185c700857156ffd03eddefdaa1549582b
岸本優一 <yk2220s@yk2220s-macmini23.local>
2023-07-24 00:42:47 +0900

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -105,6 +105,9 @@ return {
       'williamboman/mason-lspconfig.nvim',
       'hrsh7th/cmp-nvim-lsp',
       { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      -- TODO: null-ls will be archived, try alternatives
+      'jose-elias-alvarez/null-ls.nvim',
+      'jay-babu/mason-null-ls.nvim',
     },
     config = function()
       require('mason').setup()
@@ -184,6 +187,30 @@ return {
           settings = v,
         })
       end
+
+      -- null-ls
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      require("null-ls").setup({
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format()
+              end,
+            })
+          end
+        end,
+      })
+
+      require("mason-null-ls").setup({
+        ensure_installed = { "gofumpt" },
+        automatic_setup = true,
+        automatic_installation = false,
+        handlers = {},
+      })
     end,
   },
   {
